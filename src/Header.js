2ed4db624c04e926e9348a860a8ff3f708577070
Gabzk <gabrielrodriguesalves693@gmail.com>
2023-12-21 18:01:07 -0300

    MarkUp

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.png";
+
+export default function Header() {
+	return (
+		<header>
+			<nav className="nav">
+				<img src={logo} alt="Logo" className="nav-logo" />
+				<ul className="nav-items">
+					<li>Pricing</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./Header.js"
+import Footer from "./Footer.js"
+import MainContent from "./MainContent.js";
+import "./style.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <h1>Hello World!</h1>
-);
+function App() {
+	return (
+		<div>
+			<Header />
+			<MainContent />
+			<Footer />
+		</div>
+	);
+}
+
+root.render(<App />);
